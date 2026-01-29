7cf8cb2b13e3a76f4513d6c600726b7cdcaeda85
Vinchaylim <vinchay.lim@student.cadt.edu.kh>
2026-01-29 11:20:54 +0700

    fifth commit

--- ex4 - Filter map reduce operations.js
+++ ex4 - Filter map reduce operations.js
@@ -3,19 +3,21 @@ let numbers = [1, 2, 3, 4, 5];
 // Write the function to filter out even numbers from an array
 // You need to use the array.filter() function
 function filterEvenNumbers(arr) {
-  // TODO - Your code here (1 line)
+return arr.filter(num => num%2 == 0);
+
 }
 
 // Function to map array elements to their squared values
 // You need to use the array.map() function
 function squareNumbers(arr) {
-  //  TODO - Your code here (1 line)
+return arr.map(num => num*num );
 }
 
 // Function to compute the sum of array elements
 // You need to use the array.reduce() function
 function sumArray(arr) {
   //  TODO - Your code here (1 line)
+return arr.reduce((total, num) => total += num);
 }
 
 console.log("Even numbers:", filterEvenNumbers(numbers)); // [2, 4]
